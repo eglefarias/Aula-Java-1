31cbdde63a74f9c20ce47ae8cbef41760d1496be
Egle Farias <eglenib@gmail.com>
2022-05-01 19:53:26 -0300

    Primeiro commit

---
+++ src/Fundamentos/HelloWord.java
@@ -0,0 +1,34 @@
+package Fundamentos;
+
+public class HelloWord {
+	public static void main(String[] args) {
+		Integer NmAgencia;
+		
+		int NumCont;
+		
+		Integer DigitoConta;
+		
+		String NmTitular;
+		
+		String Saldo;
+		
+		
+		
+		
+		
+		NmAgencia = 99;
+		
+		NumCont = 999999;
+		
+		DigitoConta = 99;
+		
+		NmTitular = "Egle Farias";
+		
+		Saldo = "1.000,00";
+		
+		System.out.println("O nome do cliente é: " + NmTitular + " E sua conta é: " + NumCont + " Saldo em conta: " + Saldo);
+		
+	}
+
+}
+
